7dab1ccdb3a775d9cfe61065569747c19df89cad
mehrajuddin.malik@externals.hilti.com <mehraj.malik007@gmail.com>
2019-02-14 17:34:25 +0530

    getAll from DB

--- student/src/main/java/com/student/service/StudentService.java
+++ student/src/main/java/com/student/service/StudentService.java
@@ -3,7 +3,6 @@ package com.student.service;
 import com.student.model.Student;
 import com.student.repository.StudentRepository;
 import com.student.request.StudentRequest;
-import com.student.util.Util;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -15,11 +14,11 @@ public class StudentService {
 
     private StudentRepository studentRepository;
 
-    public List<Student> findAllStudents(){
-        return Util.provideStudents();
+    public List<Student> findAllStudents() {
+        return studentRepository.findAll();
     }
 
-    public Student createStudent(StudentRequest request){
+    public Student createStudent(StudentRequest request) {
         Student student = new Student();
         student.setName(request.getName());
         Student savedStudent = studentRepository.save(student);
